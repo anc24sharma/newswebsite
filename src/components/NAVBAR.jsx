2a895c641ef771d232d434f471a781d30d55ca1d
anc24sharma <ancdto123@gmail.com>
2025-03-10 12:02:56 +0530

    Update NAVBAR.jsx

--- src/components/NAVBAR.jsx
+++ src/components/NAVBAR.jsx
@@ -6,10 +6,10 @@ const NAVBAR = () => {
     <nav className='navbar'>
       <div className='logo'>News<span className= 'logo1'>BIT</span></div>
       <ul className='nav-links'>
-        <li><a href="">home</a></li>
-        <li><a href="">about</a></li>
-        <li><a href="">news</a></li>
-        <li><a href="">contact</a></li>
+        <li><a href="/">home</a></li>
+        <li><a href="#about">about</a></li>
+        <li><a href="#news">news</a></li>
+        <li><a href="#footer">contact</a></li>
       </ul>
     </nav>
   )
